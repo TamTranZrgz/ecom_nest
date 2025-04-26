80609b4070157ac52b79ebea76457c130ce68670
TamTranZrgz <thanh.tam.tran.zrgz@gmail.com>
2025-04-26 02:26:30 +0200

    set up swagger - create swagger yaml file for testing

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - Pattern: repository pattern
 - Send email: Resend (only in sandbox environment)
 - Create html email template: postdrop
+- Oauth Login
+- Swagger OpenAPI
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors() // Allow all origins for development
   await app.listen(process.env.PORT ?? 4000)
 }
 bootstrap()
